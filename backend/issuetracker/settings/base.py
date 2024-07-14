26a182df50569b1431053c75d9dbbdddf91b0c48
Ryan <sailantht@gmail.com>
2024-07-14 08:36:32 +0330

    next: Handle form submition

--- backend/issuetracker/settings/base.py
+++ backend/issuetracker/settings/base.py
@@ -42,7 +42,7 @@ LOCAL_APPS = [
 THIRD_PARTY_APPS = [
     "rest_framework",
     # "django_filters",
-    # "corsheaders",
+    "corsheaders",
     # "drf_spectacular",
     "debug_toolbar",
     # "djoser",
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -76,6 +77,11 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
 
 TEMPLATES = [
     {
--- frontend/app/issues/new/page.tsx
+++ frontend/app/issues/new/page.tsx
@@ -1,15 +1,43 @@
 "use client";
 import { Button, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
 
+import SimpleMDE from "react-simplemde-editor";
+
+import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <SimpleMDE placeholder="Description" />
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("http://127.0.0.1:8000/api/issue/create/", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root
+        placeholder="Title"
+        {...register("title", { required: true })}
+      />
+
+      <Controller
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+        name="description"
+        control={control}
+      />
+
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
