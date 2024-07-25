df0ccb7ae8c7e9cc9c9adbb8d42a0a65f6dab8fb
Ryan <sailantht@gmail.com>
2024-07-25 09:20:58 +0330

    Refactor: Apply the SRP

--- frontend/app/issues/[id]/page.tsx
+++ frontend/app/issues/[id]/page.tsx
@@ -2,12 +2,10 @@
 
 import useIssue from "@/app/hooks/useIssue";
 import { notFound } from "next/navigation";
-import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
-import ReactMarkdown from "react-markdown";
+import { Box, Grid } from "@radix-ui/themes";
 import LoadingIssueDetailPage from "@/app/issues/[id]/loading";
-import { Pencil2Icon } from "@radix-ui/react-icons";
-import Link from "next/link";
+import EditIssueButton from "@/app/issues/[id]/EditIssueButton";
+import IssueDetails from "@/app/issues/[id]/IssueDetails";
 
 interface Props {
   params: { id: string };
@@ -27,20 +25,10 @@ const IssueDetailPage = ({ params }: Props) => {
   return (
     <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Box>
-        <Heading>{issue?.title}</Heading>
-        <Flex gap="3" align="center" my="2">
-          <IssueStatusBadge status={issue.status} />
-          <Text>{issue?.created_at}</Text>
-        </Flex>
-        <Card className="prose mt-4">
-          <ReactMarkdown>{issue?.description}</ReactMarkdown>
-        </Card>
+        <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-        </Button>
+        <EditIssueButton issueId={issue.id} />
       </Box>
     </Grid>
   );
