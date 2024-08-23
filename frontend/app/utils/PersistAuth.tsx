1c22ac776ba13b2d4dedffa46521cd45eb1b99b6
Ryan <sailantht@gmail.com>
2024-08-23 10:59:29 +0330

    Get new access token

--- frontend/app/utils/PersistAuth.tsx
+++ frontend/app/utils/PersistAuth.tsx
@@ -1,36 +1,47 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useAuthStore from "@/app/store";
 import AuthClient from "@/app/services/auth-client";
 import { UserEmail } from "@/app/entities/User";
 import { getCookie } from "cookies-next";
+import { AxiosError } from "axios";
 
-const authClient = new AuthClient<UserEmail>("/check/");
+const checkUserClient = new AuthClient<UserEmail>("/check/");
+const refreshTokenClient = new AuthClient<string>("/jwt/refresh/");
 
+// PersistAuth component: Checks and maintains user authentication state
 const PersistAuth = () => {
   const { login, logout } = useAuthStore();
 
   useEffect(() => {
     const isLoggedIn = getCookie("logged_in") === "true";
     if (isLoggedIn) {
-      const checkAuth = async () => {
-        try {
-          const response = await authClient.get();
-
-          if (response.isAuthenticated) {
-            login(response.user);
-          } else {
-            logout();
-          }
-        } catch (error) {
-          console.error("Error checking authentication:", error);
-          logout();
-        }
-      };
-
       void checkAuth();
     }
   }, [login, logout]);
+
+  const checkAuth = async () => {
+    try {
+      const response = await checkUserClient.get();
+      if (response.isAuthenticated) {
+        login(response.user);
+      } else {
+        logout();
+      }
+    } catch (error) {
+      if (error instanceof AxiosError && error?.response?.status === 401) {
+        try {
+          await refreshTokenClient.post();
+          await checkAuth();
+        } catch (refreshError) {
+          logout();
+        }
+      } else {
+        logout();
+      }
+    }
+  };
+
   return null;
 };
 
