984ab619ae3ec42b0fbea97ca2785be05fbcf174
Ryan <sailantht@gmail.com>
2024-09-01 18:07:43 +0330

    Lay out the Dashboard

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,20 +3,28 @@ import LatestIssue from "@/app/LatestIssue";
 import IssueSummary from "@/app/IssueSummary";
 import useIssueStatusCount from "@/app/hooks/issues/useIssueStatusCount";
 import IssueChart from "@/app/IssueChart";
+import { Flex, Grid } from "@radix-ui/themes";
 
 export default function Home() {
   const { data: status } = useIssueStatusCount();
 
   return (
-    <>
-      <LatestIssue />
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
       {status && (
-        <IssueChart
-          open={status.OPEN}
-          inProgress={status.IN_PROGRESS}
-          closed={status.CLOSED}
-        />
+        <Flex direction="column" gap="6">
+          <IssueSummary
+            open={status.OPEN}
+            inProgress={status.IN_PROGRESS}
+            closed={status.CLOSED}
+          />
+          <IssueChart
+            open={status.OPEN}
+            inProgress={status.IN_PROGRESS}
+            closed={status.CLOSED}
+          />
+        </Flex>
       )}
-    </>
+      <LatestIssue />
+    </Grid>
   );
 }
